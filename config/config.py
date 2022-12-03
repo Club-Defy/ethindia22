88181eab136314e68ad41dcaacc8e89ae1d64b0e
ketan <shubhketanagarwal@Shubhs-MacBook-Pro.local>
2022-12-03 19:22:33 +0530

    accept 98 token symbols

--- config/config.py
+++ config/config.py
@@ -13,7 +13,12 @@ with open(os.path.join(os.path.dirname(__file__), 'config.yml'), 'r') as file:
 
     w3 = web3.Web3(web3.HTTPProvider(ymlConfig['node_address']))
     bot_token = ymlConfig["bot"]["token"]
+
     with open(ymlConfig["abi_path"]["erc20"], 'r') as erc20:
         abi_erc20 = json.load(erc20)
     with open(ymlConfig["abi_path"]["erc721"], 'r') as erc721:
         abi_erc721 = json.load(erc721)
+    with open(ymlConfig["address_by_erc20_symbol_file"], 'r') as address_by_symbol:
+        file_name = address_by_symbol.name
+        with open(file_name, 'r') as address_file:
+            address_by_erc20_symbol = yaml.safe_load(address_file)
--- services/bot/transfer.py
+++ services/bot/transfer.py
@@ -4,16 +4,18 @@ import math
 
 from web3 import Web3
 
-from config.config import w3, abi_erc20, baseUrl
+from config.config import w3, abi_erc20, baseUrl, address_by_erc20_symbol
 
 
 def eth(to_address, amount):
-    return _get_url('eth', to_address, '0x0', Web3.toWei(amount, 'ether'))
+    return _get_url('eth', to_address, '0x0000000000000000000000000000000000000000', Web3.toWei(amount, 'ether'))
 
 
 def erc20(to_address, token_address, amount):
-    return _get_url('erc20', to_address, token_address,
-                    int(amount) * math.pow(10, w3.eth.contract(address=Web3.toChecksumAddress(token_address), abi=abi_erc20).functions.decimals().call()))
+    token = _get_erc20_address(token_address)
+    return _get_url('erc20', to_address, token,
+                    int(amount) * math.pow(10, w3.eth.contract(address=Web3.toChecksumAddress(token),
+                                                               abi=abi_erc20).functions.decimals().call()))
 
 
 def erc721(to_address, token_address, token_id):
@@ -34,3 +36,10 @@ def _get_return_value(action, to_address, contract, value):
             'to_address': Web3.toChecksumAddress(to_address)
         }
     }
+
+
+def _get_erc20_address(token_name):
+    token = str.upper(token_name)
+    if token not in address_by_erc20_symbol:
+        return token_name
+    return address_by_erc20_symbol[token]
