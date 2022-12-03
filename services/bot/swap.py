ee54431d4816bc188bdbd4c3e51726274dfeae6d
Harsha Vardhan <harsha@clubwalrus.com>
2022-12-03 20:18:20 +0530

    Rename var

--- services/bot/swap.py
+++ services/bot/swap.py
@@ -4,7 +4,7 @@ import json
 import base64
 import math
 
-from config.config import w3, abi_erc20, baseUrl, transaction_deadline_min, uniswap_router_address, weth_address
+from config.config import w3, abi_erc20, baseUrl, transaction_deadline_mins, uniswap_router_address, weth_address
 
 
 def swap_erc20_to_erc20(from_amount, from_currency, to_currency):
@@ -26,7 +26,7 @@ def _get_base_amount(amount, contract_address):
 
 
 def _fetch_deadline():
-    return int(time.time()) + 60 * transaction_deadline_min
+    return int(time.time()) + 60 * transaction_deadline_mins
 
 
 def _get_url(action, contract_address, value, amount_in, amount_out_min, path, deadline):
