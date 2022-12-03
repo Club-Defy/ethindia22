d3fb104f0a85fc2e4537d3679b16d1e1e80f66ae
Parth Ghadge <parth@defyclub.com>
2022-12-03 23:15:50 +0530

    Added discord id support for transfers

--- bot/eth_speaker.py
+++ bot/eth_speaker.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from config.config import baseUrl, bot_token
 from services.bot.register import register_user
 from services.bot.swap import swap_currency
-from services.bot.transfer import erc721, erc20, eth
+from services.bot.transfer import erc721, erc20, eth, get_address_from_id
 
 description = '''This bot goes on to talk to the chain to get things done'''
 
@@ -32,8 +32,26 @@ async def transfer_eth(ctx, to_address, amount):
 
 
 @bot.command()
+async def transfer_eth_discord(ctx, member: discord.User, amount):
+    to_address = get_address_from_id(member.id)
+    if to_address == "":
+        await ctx.send("Discord user not registered with valid ethereum address")
+    else:
+        await ctx.send(eth(to_address, amount))
+
+
+@bot.command()
 async def transfer_nft(ctx, token_address, to_address, token_id):
     await ctx.send(erc721(to_address, token_address, token_id))
+
+
+@bot.command()
+async def transfer_nft_discord(ctx, token_address, member: discord.User, token_id):
+    to_address = get_address_from_id(member.id)
+    if to_address == "":
+        await ctx.send("Discord user not registered with valid ethereum address")
+    else:
+        await ctx.send(erc721(to_address, token_address, token_id))
 
 
 @bot.command()
@@ -42,8 +60,18 @@ async def transfer_erc20(ctx, token_address, to_address, amount):
 
 
 @bot.command()
+async def transfer_erc20_discord(ctx, token_address, member: discord.User, amount):
+    to_address = get_address_from_id(member.id)
+    if to_address == "":
+        await ctx.send("Discord user not registered with valid ethereum address")
+    else:
+        await ctx.send(erc20(to_address, token_address, amount))
+
+
+@bot.command()
 async def swap(ctx, from_currency, to_currency, from_amount):
     swap_currency(from_currency, to_currency, from_amount)
     await ctx.send(baseUrl + from_amount + " " + from_currency + " to " + to_currency)
+
 
 bot.run(bot_token)
--- services/bot/transfer.py
+++ services/bot/transfer.py
@@ -1,10 +1,12 @@
 import base64
 import json
 import math
+import os
 
+import yaml
 from web3 import Web3
 
-from config.config import w3, abi_erc20, baseUrl
+from config.config import w3, abi_erc20, baseUrl, id_address_map_file
 
 
 def eth(to_address, amount):
@@ -13,7 +15,8 @@ def eth(to_address, amount):
 
 def erc20(to_address, token_address, amount):
     return _get_url('erc20', to_address, token_address,
-                    int(amount) * math.pow(10, w3.eth.contract(address=Web3.toChecksumAddress(token_address), abi=abi_erc20).functions.decimals().call()))
+                    int(amount) * math.pow(10, w3.eth.contract(address=Web3.toChecksumAddress(token_address),
+                                                               abi=abi_erc20).functions.decimals().call()))
 
 
 def erc721(to_address, token_address, token_id):
@@ -34,3 +37,17 @@ def _get_return_value(action, to_address, contract, value):
             'to_address': Web3.toChecksumAddress(to_address)
         }
     }
+
+
+def get_address_from_id(discord_id):
+    mapping_file = id_address_map_file
+    directory_name = os.path.dirname
+    mapping_file_path = os.path.join(directory_name(directory_name(directory_name(__file__))), mapping_file)
+    if not os.path.exists(mapping_file_path): return ""
+
+    stream = open(mapping_file_path, 'r')
+    data = yaml.safe_load(stream)
+    if data is None:
+        return ""
+    else:
+        return data[discord_id]
