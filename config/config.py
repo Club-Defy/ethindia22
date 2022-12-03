4e1298eeec9747d8697df2b4302c90c40d07a85b
Harsha Vardhan <harsha@clubwalrus.com>
2022-12-03 18:56:59 +0530

    Add swap responses

---
+++ config/config.py
@@ -0,0 +1,23 @@
+import json
+
+import web3
+import yaml
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'config.yml'), 'r') as file:
+    ymlConfig = yaml.safe_load(file)
+    env = ymlConfig["env"]
+    health_check_response = ymlConfig["health_check_msg"]
+    id_address_map_file = ymlConfig["id_address_mapping_filename"]
+    baseUrl = ymlConfig["baseUrl"]
+
+    w3 = web3.Web3(web3.HTTPProvider(ymlConfig['node_address']))
+    bot_token = ymlConfig["bot"]["token"]
+    with open(ymlConfig["abi_path"]["erc20"], 'r') as erc20:
+        abi_erc20 = json.load(erc20)
+    with open(ymlConfig["abi_path"]["erc721"], 'r') as erc721:
+        abi_erc721 = json.load(erc721)
+
+    transaction_deadline_min = ymlConfig["transaction_deadline_min"]
+    uniswap_router_address = ymlConfig["uniswap_router_address"]
+    weth_address = ymlConfig["weth_address"]
