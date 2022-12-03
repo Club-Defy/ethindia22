0618597245cd08e28d7f49bc4fa36fef1137e020
Shubh Ketan Agarwal <shubhketanagarwal@Shubhs-MacBook-Pro.local>
2022-12-03 15:57:17 +0530

    transfer erc20 and erc721

--- bot/EthSpeaker.py
+++ bot/EthSpeaker.py
@@ -5,7 +5,7 @@ from services.bot.register import register_user
 from services.bot.swap import swap_currency
 from services.bot.transfer import erc721, erc20, eth
 
-description = '''This bit goes on to talk to the chain to get things done'''
+description = '''This bot goes on to talk to the chain to get things done'''
 
 intents = discord.Intents.default()
 intents.members = False
@@ -27,27 +27,31 @@ async def register(ctx):
     register_user(ctx.author.id)
     await ctx.send("this will register users")
 
-@bot.command(description="transfers eth")
+
+@bot.command()
 async def transfer_eth(ctx, to_address, amount):
-    eth(to_address, amount)
-    await ctx.send("this will transfer " + amount + " eth to " + to_address)
+    await ctx.send("https://link.io?q=" + eth(to_address, amount))
+
+
+@bot.command()
+async def approve_nft(ctx, token_address, to_address, token_id):
+    await ctx.send("https://link.io?q=" + approve_erc721(to_address, token_address, token_id))
 
 
 @bot.command()
 async def transfer_nft(ctx, token_address, to_address, token_id):
-    erc721(to_address, token_address, token_id)
-    await ctx.send("(nft) this will transfer " + token_id + " " + token_address + " to " + to_address)
+    await ctx.send("https://link.io?q=" + erc721(to_address, token_address, token_id))
 
 
 @bot.command()
 async def transfer_erc20(ctx, token_address, to_address, amount):
-    erc20(to_address, token_address, amount)
-    await ctx.send("(erc20) this will transfer " + amount + " " + token_address + " to " + to_address)
+    await ctx.send("https://link.io?q=" + erc20(to_address, token_address, amount))
 
 
 @bot.command()
 async def swap(ctx, from_currency, to_currency, from_amount):
     swap_currency(from_currency, to_currency, from_amount)
     await ctx.send("this will swap " + from_amount + " " + from_currency + " to " + to_currency)
+
 
 bot.run(token)
--- services/bot/transfer.py
+++ services/bot/transfer.py
@@ -1,12 +1,31 @@
+import base64
+import json
+
+
 def eth(to_address, amount):
     print(to_address, " -> ", amount)
     pass
 
 
 def erc20(to_address, token_address, amount):
-    print("transfer " + amount + " " + token_address + " to " + to_address)
-    pass
+    return _get_url(to_address, token_address, amount)
+
 
 def erc721(to_address, token_address, token_id):
-    print("transfer " + token_id + " " + token_address + " to " + to_address)
-    pass
+    return _get_url(to_address, token_address, token_id)
+
+
+def _get_url(to_address, token_address, token_id):
+    return base64.urlsafe_b64encode(
+        json.dumps(_get_return_value(to_address, token_address, token_id)).encode()).decode()
+
+
+def _get_return_value(action, to_address, contract, value):
+    return {
+        'action': action,
+        'collection_address': contract,
+        'params': {
+            'value': value,
+            'to_address': to_address
+        }
+    }
