02ee2b34b2a74d055e2c10e4ff12ed159628aa85
Shubh Ketan Agarwal <shubhketanagarwal@Shubhs-MacBook-Pro.local>
2022-12-03 12:04:10 +0530

    add servies layer

---
+++ services/bot/register.py
@@ -0,0 +1,4 @@
+def register_user(user_id):
+    #do user registration here
+    print(user_id)
+    pass
---
+++ services/bot/swap.py
@@ -0,0 +1,3 @@
+def swap_currency(from_currency, to_currency, from_amount):
+    print("swap " + from_amount + " " + from_currency + " to " + to_currency)
+    pass
