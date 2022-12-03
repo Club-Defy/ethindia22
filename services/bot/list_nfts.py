9a51389b4817945e2578bb94ec4b71e543f0dd15
Parth Ghadge <parth@defyclub.com>
2022-12-03 21:36:57 +0530

    set thumbnail

--- services/bot/list_nfts.py
+++ services/bot/list_nfts.py
@@ -17,7 +17,8 @@ def list_erc721(address):
             embed = discord.Embed(
                 title=nft["contract"]["address"],
                 url=nft["media"][0]["gateway"])\
-                .add_field(name="TokenId", value=nft["id"]["tokenId"])
+                .add_field(name="TokenId", value=nft["id"]["tokenId"])\
+                .set_thumbnail(url=nft["media"][0]["gateway"])
             nft_list_response.append(embed)
 
     return nft_list_response
