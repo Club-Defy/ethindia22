a270a60c4df6b2e89676c4e8f6d7566d463cf2a9
ketan <shubhketanagarwal@Shubhs-MacBook-Pro.local>
2022-12-03 16:16:26 +0530

    fix eth and addresses

--- services/bot/transfer.py
+++ services/bot/transfer.py
@@ -1,31 +1,33 @@
 import base64
 import json
 
+from web3 import Web3
+
 
 def eth(to_address, amount):
-    print(to_address, " -> ", amount)
-    pass
+    return _get_url('eth', to_address, '0x0', Web3.toWei(amount, 'ether'))
 
 
 def erc20(to_address, token_address, amount):
-    return _get_url(to_address, token_address, amount)
+    return _get_url('erc20', to_address, token_address, amount)
 
 
 def erc721(to_address, token_address, token_id):
-    return _get_url(to_address, token_address, token_id)
+    return _get_url('erc721', to_address, token_address, token_id)
 
 
-def _get_url(to_address, token_address, token_id):
+def _get_url(action, to_address, token_address, token_id):
+    print(token_id)
     return base64.urlsafe_b64encode(
-        json.dumps(_get_return_value(to_address, token_address, token_id)).encode()).decode()
+        json.dumps(_get_return_value(action, to_address, token_address, token_id)).encode()).decode()
 
 
 def _get_return_value(action, to_address, contract, value):
     return {
         'action': action,
-        'collection_address': contract,
+        'collection_address': Web3.toChecksumAddress(contract),
         'params': {
             'value': value,
-            'to_address': to_address
+            'to_address': Web3.toChecksumAddress(to_address)
         }
     }
